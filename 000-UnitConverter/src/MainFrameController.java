265836319d76ebe06fd3570ec32d7c53aa0bc007
CharlieCharl <karol.jamruszka@gmail.com>
2019-01-25 16:40:42 +0100

    init commit for BMI calculator

--- 000-UnitConverter/src/MainFrameController.java
+++ 000-UnitConverter/src/MainFrameController.java
@@ -100,7 +100,6 @@ public class MainFrameController {
                    mainFrame.getLengthUnitsTo().setListData(weightUnits);
                    break;
            }
-
         }
     }
 }
---
+++ 006-BMICalculator/src/BmiCalculatorView.java
@@ -0,0 +1,4 @@
+import javax.swing.*;
+
+public class BmiCalculatorView extends JFrame {
+}
