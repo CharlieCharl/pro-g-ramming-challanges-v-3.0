70f9625667806314a8d6e679142f4901d78de33c
CharlieCharl <karol.jamruszka@gmail.com>
2019-02-05 21:21:10 +0100

    set window size and title

--- 006-BMICalculator/src/BmiCalculatorView.java
+++ 006-BMICalculator/src/BmiCalculatorView.java
@@ -1,4 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class BmiCalculatorView extends JFrame {
+
+    public BmiCalculatorView(){
+        this.setTitle("BMI calculator");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        GridBagLayout layout = new GridBagLayout();
+        setLayout(layout);
+        this.setSize(400,300);
+    }
 }
