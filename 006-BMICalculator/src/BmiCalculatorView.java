d7b95f254dcd843e01681161d70a928fcf770678
CharlieCharl <karol.jamruszka@gmail.com>
2019-02-06 23:56:49 +0100

    add textfileds and labels for view

--- 006-BMICalculator/src/BmiCalculatorView.java
+++ 006-BMICalculator/src/BmiCalculatorView.java
@@ -6,24 +6,37 @@ public class BmiCalculatorView extends JFrame {
     JRadioButton menButton;
     JRadioButton womanButton;
     ButtonGroup genderGroup;
+    JLabel heightLabel;
+    JLabel weightLabel;
+    JTextField heightTextField;
+    JTextField weightTextField;
 
     public BmiCalculatorView(){
         this.setTitle("BMI calculator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        GridBagLayout layout = new GridBagLayout();
+        GridLayout layout = new GridLayout(4,2);
         setLayout(layout);
         this.setSize(400,300);
 
         menButton = new JRadioButton("Men");
         womanButton = new JRadioButton("Woman");
 
+        heightLabel = new JLabel("Height:");
+        weightLabel = new JLabel("Weight");
+
+        heightTextField = new JTextField();
+        weightTextField = new JTextField();
+
         Container c = getContentPane();
 
         c.add(menButton);
         c.add(womanButton);
+        c.add(heightLabel);
+        c.add(heightTextField);
+        c.add(weightLabel);
+        c.add(weightTextField);
 
         genderGroup = new ButtonGroup();
         genderGroup.add(menButton);
